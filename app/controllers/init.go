0aefb69ee7aee5d8063a5c32034156861c5b2b37
Julian Horvat <julian.m.horvat@gmail.com>
2013-09-11 22:58:38 -0400

    More templating, question link now leads to a unique URI next step is create something at that route. Will probably have to look at refactoring question structure to ref tags in a tag collection rather than embedding

--- app/controllers/init.go
+++ app/controllers/init.go
@@ -1,9 +1,15 @@
 package controllers
 
-import "github.com/robfig/revel"
+import (
+	"github.com/robfig/revel"
+	bson "labix.org/v2/mgo/bson"
+)
 
-//Setup request interception
+//Setup request interception and template function
 func init() {
 	revel.InterceptMethod((*MgoController).Begin, revel.BEFORE)
 	revel.InterceptMethod((*MgoController).Close, revel.AFTER)
+
+	//Extract the hex ID of a mongo id in our template
+	revel.TemplateFuncs["bsonID"] = func(a bson.ObjectId) string { return a.Hex() }
 }
--- app/models/question.go
+++ app/models/question.go
@@ -4,6 +4,7 @@ import (
 	bson "labix.org/v2/mgo/bson"
 )
 
+//A question model that contains all of the relevant fields for a question
 type Question struct {
 	Id    bson.ObjectId "_id,omitempty"
 	Title string        "title"
